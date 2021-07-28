8e5812d9594bf2addaedf41ef0bc0b5f8a473199
prateek <prateekhajare345@gmail.com>
2021-07-28 14:46:47 +0530

    ui fixed-choices

--- lib/pages/choices.dart
+++ lib/pages/choices.dart
@@ -4,8 +4,7 @@ import 'package:flutter/material.dart';
 
 //global variables required for declaration of values
 dynamic chosenValues = [];
-late bool isChoosed;
-
+bool randomvalue = true;
 
 class Choices extends StatefulWidget {
   @override
@@ -26,34 +25,105 @@ class _ChoicesState extends State<Choices> {
 
   @override
   Widget build(BuildContext context) {  
+    bool relationship = false;
+    bool career = false;
+    bool medical = false;
+    bool parental = false;
+
     return Scaffold(
       appBar: AppBar(title: Text("Compassion"),),
-      body: ListView(
-        children: <Widget>[
-          //remember to add leading check box in this
-          CheckboxListTile(title: Text("relationship problems"), value: isChoosed,
-           secondary: Icon(Icons.favorite, color:Colors.red) ,onChanged: (bool? value) {
-             var _checked = value;
-             String val = "rel_problems";
-             if (_checked ==  true){
-               chosenValues.add(val);
-             }else{chosenValues.remove(val);}
-           },),
-          CheckboxListTile(title: Text(""), value: isChoosed,
-           secondary: Icon(Icons.favorite, color:Colors.red) ,onChanged: (bool? value) {},),
-          CheckboxListTile(title: Text(""), value: isChoosed,
-           secondary: Icon(Icons.favorite, color:Colors.red) ,onChanged: (bool? value) {},)
-
-
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text("Choose the topics you wanna talk about to someone",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Card(
+              child: ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  //remember to add leading check box in this
+                  CheckboxListTile(title: Text("relationship problems"), value: relationship,
+                   secondary: Icon(Icons.favorite, color:Colors.red) ,onChanged: (bool? value) {
+                     setState(){
+                       relationship == true?relationship=false:relationship=true;
+                     }
+                   },),
+      //----------------------------------------------------------------------------------                 
+      
+                  CheckboxListTile(title: Text("clinical/neurological problems"), value: career, 
+                   secondary: Icon(Icons.local_hospital, color:Colors.brown) ,onChanged: (bool? value) {
+                    setState(() {
+                       career==false?career=true:career=false;
+                    });
+                   },
+                   ),
+      //----------------------------------------------------------------------------------                 
+                  
+                  CheckboxListTile(title: Text("poverty/financial problems"), value: randomvalue,
+                   secondary: Icon(Icons.account_balance, color:Colors.green) ,onChanged: (bool? value) {},),
+      //----------------------------------------------------------------------------------
+                  CheckboxListTile(title: Text("Social Anxiety related problems"), value: career, 
+                   secondary: Icon(Icons.groups, color:Colors.brown) ,onChanged: (bool? value) {
+                    setState(() {
+                       career==false?career=true:career=false;
+                    });
+                   },),
+      //----------------------------------------------------------------------------------                 
+      
+                   CheckboxListTile(title: Text("Existential problems"), value: career, 
+                   secondary: Icon(Icons.sentiment_dissatisfied, color:Colors.red) ,onChanged: (bool? value) {
+                    setState(() {
+                       career==false?career=true:career=false;
+                    });
+                   },),
+                   CheckboxListTile(title: Text("career related problems"), value: career, 
+                   secondary: Icon(Icons.trending_up, color:Colors.black) ,onChanged: (bool? value) {
+                    setState(() {
+                       career==false?career=true:career=false;
+                    });
+                   },),
+      //----------------------------------------------------------------------------------                 
+      
+                   CheckboxListTile(title: Text("parental problems"), value: career, 
+                   secondary: Icon(Icons.supervisor_account, color:Colors.indigo) ,onChanged: (bool? value) {
+                    setState(() {
+                       career==false?career=true:career=false;
+                    });
+                   },),
+      //----------------------------------------------------------------------------------                 
+      
+                  CheckboxListTile(title: Text("casually talk to someone compassionaite"), value: career, 
+                   secondary: Icon(Icons.self_improvement, color:Colors.blue) ,onChanged: (bool? value) {
+                    setState(() {
+                       career==false?career=true:career=false;
+                    });
+                   },),
+      //----------------------------------------------------------------------------------                 
+                  
+                ],
+              ),
+            ),
+      //----------------------------------------------------------------------------------                 
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: MaterialButton(
+                padding: EdgeInsets.all(13),
+                onPressed: (){},
+                elevation: 15,
+                child: Text("Go"),
+                color: Colors.redAccent,
+              ),
+            ),
+          ],
+        ),
       ),
       
     );
   }
 }
-
-
-//------------------------------------------------checkbox----------------------------
-// Widget checkboxTile(String title){
-//   return MaterialButton(onPressed: (){});
-// }
--- lib/utilities/homecard.dart
+++ lib/utilities/homecard.dart
@@ -1,22 +1,65 @@
-
 import 'package:flutter/material.dart';
 
-// class homeCard extends StatefulWidget {
 
-//   @override
-//   _homeCardState createState() => _homeCardState();
-// }
 
-// class _homeCardState extends State<homeCard> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return (); 
-   
-// }
+class homeCard1 extends StatefulWidget {
+//   
+  @override
+  _homeCard1State createState() => _homeCard1State();
+}
 
-// }
+class _homeCard1State extends State<homeCard1> {
+  // creating the constructor
+  late String title;
+  late String context1; 
+  late String image;
+  homeCard23(String title, String context1, String image){
+    title = title;
+    context1 = context1;
+    image = image;
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(44)),
+    elevation: 14,
+    child: Container(
+      padding: EdgeInsets.all(85),
+      margin: EdgeInsets.all(0),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(24),
+      image: DecorationImage(image: NetworkImage("$image")
+      ,fit: BoxFit.cover
+      ),
+      ),
+      child: Column(children: <Widget>[
+        Text(
+          "$title",
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Center(
+          child: Text("$context1", textAlign: TextAlign.center),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        MaterialButton(
+            child: Text("Go"), onPressed: () {
+              //figure out how to pass parameteres into stateful widget towmmorow
+                Navigator.restorablePushReplacementNamed(context, "/choices");
+            },
+             color: Colors.pinkAccent)
+      ]),
+    ),
+  );
+  }
+}
 
-Widget homeCard(String title, String context1, String image){
+
+Widget homeCard(String title, String context1, String image, context) {
   return Card(
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(44)),
     elevation: 14,
@@ -44,6 +87,7 @@ Widget homeCard(String title, String context1, String image){
         ),
         MaterialButton(
             child: Text("Go"), onPressed: () {
+              //figure out how to pass parameteres into stateful widget towmmorow
                 Navigator.restorablePushReplacementNamed(context, "/choices");
             },
              color: Colors.pinkAccent)
